02b3815721944854e8053f030d41e999d4132e63
GregJe13 <126569840+GregJe13@users.noreply.github.com>
2024-12-01 21:47:08 +0700

    update login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,9 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <title>Document</title>
 </head>
-<body>
-    
+<body class="h-screen">
+    <div class="container mx-auto flex items-center justify-center h-full">
+        <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-xl">
+            <div class="flex items-center justify-center mt-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+                    <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
+                  </svg> 
+                  <h5 class="text-4xl font-bold tracking-tight text-gray-800 ml-2.5">IndoGrosir</h5>
+            </div>
+            
+                <input type="text" id="email" class="block w-full p-2 text-gray-900 border rounded-lg 
+                bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 mt-5 " placeholder="Email">
+                <input type="password" id="pass" class="block w-full p-2 text-gray-900 border rounded-lg 
+                bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 mt-3 " placeholder="Password">
+                <button type="button" class="mt-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 
+                focus:ring-gray-300 font-medium rounded-lg text-sm w-full py-2 me-2 mb-2">Login</button>
+            </a>
+        </div>
+    </div>  
 </body>
+
 </html>
